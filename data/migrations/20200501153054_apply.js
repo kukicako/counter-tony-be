d36dbdfb5d2f71826a5f06b9294f4967bc441351
Muamer Kukic <kukicako@gmail.com>
2020-05-02 22:51:30 -0700

    NEW test

--- data/migrations/20200501153054_apply.js
+++ data/migrations/20200501153054_apply.js
@@ -5,23 +5,21 @@ exports.up = function(knex) {
       tbl
         .string('name', 255)
         .notNullable()
-        .unique();
       tbl
         .string('email', 255)
         .notNullable()
-        .unique();
       tbl
       .string('rank', 255)
       .notNullable();
       tbl
       .string('steam', 255)
       .notNullable()
-      .unique();
       tbl
       .string('questions', 500)
       
       
     });
+    
   };
   
   exports.down = function(knex) {
